9aea86967af60986c3bd413aec4340dc2f401cfc
Martin Williams <martinwork@users.noreply.github.com>
2021-05-17 14:58:02 +0100

    mwtest-ble-uart

--- source/ble-uart-test.cpp
+++ source/ble-uart-test.cpp
@@ -1,58 +1,59 @@
-/*
-The MIT License (MIT)
-
-Copyright (c) 2016 British Broadcasting Corporation.
-This software is provided by Lancaster University by arrangement with the BBC.
-
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.
-*/
-
 #include "MicroBit.h"
 #include "MicroBitUARTService.h"
-
-// Define MICROBIT_CODAL to distinguish V2 CODAL from V1 DAL
-
-#ifndef MICROBIT_CODAL
-#ifdef CODAL_CONFIG_H
-#define MICROBIT_CODAL 1
-#else
-#define MICROBIT_CODAL 0
-#endif
-#endif
 
 
 MicroBit uBit;
 MicroBitUARTService* uart;
 
-ManagedString msg = R"(abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz)";
+//ManagedString msg = R"(abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz)";
+ManagedString alpha = "abcdefghijklmnopqrstuvwxyz\n";
+ManagedString msg = "  0" + alpha + "  1" + alpha + "  2" + alpha + "  3" + alpha + "  4" + alpha + "  5" + alpha + "  6" + alpha + "  7" + alpha + "  8" + alpha + "  9" + alpha;
 
-void uartSend(ManagedString s)
+
+int uartSend(ManagedString s)
 {
-  uint8_t bytes = uart->send(s);  // default mode is SYNC_SLEEP
+  // default mode is SYNC_SLEEP
+  // return value is int, to allow for error codes
+  int bytes = uart->send(s);
+  DMESG( "uartSend bytes = %d", bytes);
+  return bytes;
+}
+
+int uartSendAsync(ManagedString s)
+{
+    int length = s.length();
+    int sent = 0;
+
+    while ( sent < length)
+    {
+        int bytes = uart->send( s.substring( sent, length - sent), ASYNC);
+        if ( bytes > 0)
+        {
+            DMESG( "uartSendAsync bytes = %d", bytes);
+        }
+        if ( bytes < 0) return bytes;
+        sent += bytes;
+    }
+    return sent;
 }
 
 void onClickB(MicroBitEvent) 
 {
-  uartSend(msg);
-  uartSend("\n");
+    int sent;
+    sent = uartSend( msg);
+    DMESG( "onClickB sent = %d", sent);
+    sent = uartSend("\n");
+    DMESG( "onClickB sent = %d", sent);
 }
 
+void onClickA(MicroBitEvent) 
+{
+    int sent;
+    sent = uartSendAsync(msg);
+    DMESG( "onClickA sent = %d", sent);
+    sent = uartSendAsync("\n");
+    DMESG( "onClickA sent = %d", sent);
+}
 
 int main()
 {
@@ -63,6 +64,7 @@ int main()
     uart = new MicroBitUARTService(*uBit.ble, 90, 90);
 
     uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, onClickB);
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onClickA);
 
     // If main exits, there may still be other fibers running or registered event handlers etc.
     // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
