ef34e7466ee644dca7e861ec421346cbcfb6afc0
Martin Williams <martinwork@users.noreply.github.com>
2021-06-06 15:05:16 +0100

    test ble version

--- source/main.cpp
+++ source/main.cpp
@@ -7,6 +7,14 @@ main()
 {
     uBit.init();
 
+    MicroBitVersion v = uBit.power.getVersion();
+    ManagedString i( (int) v.i2c);
+    ManagedString d( (int) v.daplink);
+    ManagedString b( (int) v.board);
+    ManagedString msg = "i " + i + " d " + d + " b " + b + "\n";
+    uBit.serial.send( msg);
+
     while(1)
-        uBit.display.scroll("HELLO WORLD!");
+    {
+    }
 }
