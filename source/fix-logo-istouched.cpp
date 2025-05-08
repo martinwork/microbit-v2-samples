99e7588c0c416c41ade1ba5696cf165bde09f4b6
Martin Williams <martinwork@users.noreply.github.com>
2025-05-08 16:08:18 +0100

    Add uBit.logo.isPressed() test

--- source/fix-logo-istouched.cpp
+++ source/fix-logo-istouched.cpp
@@ -16,6 +16,21 @@ void onButtonA(MicroBitEvent e)
   uBit.display.clear();
 }
 
+void onButtonB(MicroBitEvent e)
+{
+  bool isTouched1 = uBit.logo.isPressed();
+  uBit.sleep(50);
+  bool isTouched2 = uBit.logo.isPressed();
+  
+  uBit.display.print( isTouched1 ? 'Y' : 'N');
+  uBit.sleep(500);
+  uBit.display.clear();
+  uBit.sleep(100);
+  uBit.display.print( isTouched2 ? 'Y' : 'N');
+  uBit.sleep(500);
+  uBit.display.clear();
+}
+
 void forever()
 {
     while (true)
@@ -29,6 +44,7 @@ int main() {
     uBit.init();
 
     uBit.messageBus.listen( MICROBIT_ID_BUTTON_A,  MICROBIT_BUTTON_EVT_CLICK, onButtonA);
+    uBit.messageBus.listen( MICROBIT_ID_BUTTON_B,  MICROBIT_BUTTON_EVT_CLICK, onButtonB);
 
     create_fiber( forever);
 
